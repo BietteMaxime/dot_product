b50ed7aae13dd08f0ce1f2056496423241ce402d
Nicolas Stinus <nicolas.stinus@gmail.com>
2016-08-30 09:38:04 +0200

    bench: run 1000 samples and show results more nicely

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -6,4 +6,4 @@ shift
 for i in $(seq $N)
 do
     taskset -c 2 $@
-done | grep 'StaticVec 02' | cut -d ' ' -f 9 | python $(dirname $0)/stat_desc.py
+done | python $(dirname $0)/stat_desc.py
--- scripts/stat_desc.py
+++ scripts/stat_desc.py
@@ -1,6 +1,23 @@
 import pandas
 import sys
 
+pandas.options.display.width = 180
+
+# DATA = """StaticVec 00 size 8192 nb_unitvecs 1024 9.15925e+10 elapsed 23661 ns
+# StaticVec 01 size 8192 nb_unitvecs 1024 9.15925e+10 elapsed 4717 ns
+# StaticVec 02 size 8192 nb_unitvecs 1024 9.15924e+10 elapsed 1988 ns
+# StaticVec 03 size 8192 nb_unitvecs 1024 9.15923e+10 elapsed 3660 ns
+# StaticVec 00 size 8192 nb_unitvecs 1024 9.15925e+10 elapsed 14357 ns
+# StaticVec 01 size 8192 nb_unitvecs 1024 9.15925e+10 elapsed 5198 ns
+# StaticVec 02 size 8192 nb_unitvecs 1024 9.15924e+10 elapsed 2681 ns
+# StaticVec 03 size 8192 nb_unitvecs 1024 9.15923e+10 elapsed 3335 ns""".splitlines()
+DATA = sys.stdin.readlines()
+
 if __name__ == "__main__":
-    data = pandas.DataFrame([float(i) for i in sys.stdin.readlines()])
-    print(data.describe())
+    data = [i.split() for i in DATA]
+    data = pandas.DataFrame([(int(i[1]), float(i[8])) for i in data], columns=["method", "ns"])
+    ret = list()
+    for method in data["method"].unique():
+        desc = data[data.method == method].describe().transpose().ix[1]
+        ret.append(pandas.Series(method, ["method",]).append(desc))
+    print(pandas.DataFrame(ret))
